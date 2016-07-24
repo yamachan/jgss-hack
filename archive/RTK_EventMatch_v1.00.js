3c72324ed398e7e46b7d7255d8c59fa95acac543
Toshio Yamashita <z@rinco.jp>
2016-07-24 18:31:43 +0900

    ver 1.01

--- archive/RTK_EventMatch_v1.00.js
+++ archive/RTK_EventMatch_v1.00.js
@@ -1,5 +1,5 @@
 ﻿//=============================================================================
-// RTK_EventMatch.js  ver1.01 2016/07/24
+// RTK_EventMatch.js  ver1.00 2016/07/24
 // The MIT License (MIT)
 //=============================================================================
 
@@ -18,10 +18,6 @@
  *   <event match_!sr:s,r>	# Switch s' condition is this event is not in r region
  *   <event match_vr:v,r>	# Add 1 to variable v, if this event is in r region
  *   <event match_!vr:v,r>	# Add 1 to variable v, if this event is not in r region
- *   <event match_se:s,e>	# Switch s' condition is this event and e event is in a same location
- *   <event match_!se:s,e>	# Switch s' condition is this event and e event is not in a same location
- *   <event match_ve:v,e>	# Add 1 to variable v, if this event and e event is in a same location
- *   <event match_!ve:v,e>	# Add 1 to variable v, if this event and e event is not in a same location
  */
 
 /*:ja
@@ -39,10 +35,6 @@
  *   <event match_!sr:s,r>	# スイッチ s の条件はこのイベントが r リージョンにないこと
  *   <event match_vr:v,r>	# このイベントが r リージョンにあれば変数 v に1を加える
  *   <event match_!vr:v,r>	# このイベントが r リージョンになければ変数 v に1を加える
- *   <event match_se:s,e>	# スイッチ s の条件はこのイベントが e イベントと同じ位置にあること
- *   <event match_!se:s,e>	# スイッチ s の条件はこのイベントが e イベントと同じ位置にないこと
- *   <event match_ve:v,e>	# このイベントが  e イベントと同じ位置にあれば変数 v に1を加える
- *   <event match_!ve:v,e>	# このイベントが  e イベントと同じ位置になければ変数 v に1を加える
  *
  * 解説ページ:
  * https://github.com/yamachan/jgss-hack/blob/master/RTK_EventMatch.ja.md
@@ -67,21 +59,24 @@
 			var ret = String(meta[tag_name + "_sr"]||"").match(/^\s*(\d+),(\d+)\s*$/);
 			if (ret) {
 				var sw = Number(ret[1]);
+				var region = Number(ret[2]);
 				if (sw > 0) {
-					sws[sw] = (sws[sw] === undefined ? true : sws[sw]) && event.regionId() == Number(ret[2]);
+					sws[sw] = sws[sw] === undefined ? event.regionId() == region : sws[sw] && event.regionId() == region;
 				}
 			}
 			ret = String(meta[tag_name + "_!sr"]||"").match(/^\s*(\d+),(\d+)\s*$/);
 			if (ret) {
 				var sw = Number(ret[1]);
+				var region = Number(ret[2]);
 				if (sw > 0) {
-					sws[sw] = (sws[sw] === undefined ? true : sws[sw]) && event.regionId() != Number(ret[2]);
+					sws[sw] = sws[sw] === undefined ? event.regionId() != region : sws[sw] && event.regionId() != region;
 				}
 			}
 			ret = String(meta[tag_name + "_vr"]||"").match(/^\s*(\d+),(\d+)\s*$/);
 			if (ret) {
 				var v = Number(ret[1]);
-				if (v > 0 && event.regionId() == Number(ret[2])) {
+				var region = Number(ret[2]);
+				if (v > 0 && event.regionId() == region) {
 					vars[v] = vars[v] || 0;
 					vars[v]++;
 				}
@@ -89,41 +84,8 @@
 			ret = String(meta[tag_name + "_!vr"]||"").match(/^\s*(\d+),(\d+)\s*$/);
 			if (ret) {
 				var v = Number(ret[1]);
-				if (v > 0 && event.regionId() != Number(ret[2])) {
-					vars[v] = vars[v] || 0;
-					vars[v]++;
-				}
-			}
-			ret = String(meta[tag_name + "_se"]||"").match(/^\s*(\d+),(\d+)\s*$/);
-			if (ret) {
-				var sw = Number(ret[1]);
-				var ev = $gameMap.event(Number(ret[2])) || {};
-				if (sw > 0) {
-					sws[sw] = (sws[sw] === undefined ? true : sws[sw]) && event.x == ev.x && event.y == ev.y;
-				}
-			}
-			ret = String(meta[tag_name + "_!se"]||"").match(/^\s*(\d+),(\d+)\s*$/);
-			if (ret) {
-				var sw = Number(ret[1]);
-				var ev = $gameMap.event(Number(ret[2])) || {};
-				if (sw > 0) {
-					sws[sw] = (sws[sw] === undefined ? true : sws[sw]) && (event.x != ev.x || event.y != ev.y);
-				}
-			}
-			ret = String(meta[tag_name + "_ve"]||"").match(/^\s*(\d+),(\d+)\s*$/);
-			if (ret) {
-				var v = Number(ret[1]);
-				var ev = $gameMap.event(Number(ret[2])) || {};
-				if (sw > 0 && event.x == ev.x && event.y == ev.y) {
-					vars[v] = vars[v] || 0;
-					vars[v]++;
-				}
-			}
-			ret = String(meta[tag_name + "_!ve"]||"").match(/^\s*(\d+),(\d+)\s*$/);
-			if (ret) {
-				var v = Number(ret[1]);
-				var ev = $gameMap.event(Number(ret[2])) || {};
-				if (sw > 0 && (event.x != ev.x || event.y != ev.y)) {
+				var region = Number(ret[2]);
+				if (v > 0 && event.regionId() != region) {
 					vars[v] = vars[v] || 0;
 					vars[v]++;
 				}
