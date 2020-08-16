5026e6cd0960e4f025871b16b57fa8a1b1818a64
yamachan <z@rinco.jp>
2020-08-16 19:24:32 +0900

    Create RTK_Test2.js
    Add plugin template2

---
+++ guide/RTK_Test2.js
@@ -0,0 +1,28 @@
+﻿//=============================================================================
+// RTK_Test2.js	2020/08/16
+// The MIT License (MIT)
+//=============================================================================
+
+/*:
+ * @plugindesc Test Plugin 2
+ * @author Toshio Yamashita (yamachan)
+ *
+ * @help This plugin does not provide plugin commands.
+ * This is for the test purpose only. So, please rename for the production use.
+ */
+
+/*:ja
+ * @plugindesc テスト用プラグイン2
+ * @author Toshio Yamashita (yamachan)
+ *
+ * @help このプラグインにはプラグインコマンドはありません。
+ * テスト用に作成したものなので、実際に利用する場合には適当にリネームしてください
+ */
+
+(function(_global) {
+	"use strict";
+	var N = document.currentScript.src.replace(/^.*\/([^/]+)\.js$/, '$1');
+	var param = PluginManager.parameters(N);
+
+	// ここにプラグイン処理を記載
+})(this);
